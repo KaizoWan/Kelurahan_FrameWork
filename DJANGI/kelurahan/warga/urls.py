bfaaac2f76efdaa0bb7a0853ab0af5349ebb2359
KaizoWan <mr0280463@gmail.com>
2025-10-15 11:38:09 +0700

    Pertemuan 3

--- DJANGI/kelurahan/warga/urls.py
+++ DJANGI/kelurahan/warga/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import WargaListView,WargaDetailView,PengaduanListView
+from .views import WargaListView, WargaDetailView, WargaCreateView, PengaduanListView, PengaduanCreateView
 
 urlpatterns = [
     path('warga', WargaListView.as_view(), name='warga_list'),
     path('<int:pk>/', WargaDetailView.as_view(), name='warga_detail'),
     path('pengaduan/', PengaduanListView.as_view(), name='pengaduan_list'),
+    path('tambah/', WargaCreateView.as_view(), name='warga_tambah'),
+    path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan_tambah'),
 ]
--- DJANGI/kelurahan/warga/views.py
+++ DJANGI/kelurahan/warga/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Warga,Pengaduan
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import WargaForm, PengaduanForm
 
 class WargaListView(ListView):
     model = Warga
@@ -10,4 +13,15 @@ class WargaDetailView(DetailView):
 
 class PengaduanListView(ListView):
     model = Pengaduan
-# Create your views here.
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga_list')
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan_list')
